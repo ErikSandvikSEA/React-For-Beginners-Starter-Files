5baaec8bfd95f0205e965d1917972949a957b7cd
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-13 19:06:18 -0700

    added change of routes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,20 @@
 import React from 'react'; 
-
+import { getFunName } from '../helpers'
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+   goToStore = (event) => {
+       event.preventDefault()
+      const storeName = this.myInput.current.value
+    
+       this.props.history.push(`/store/${storeName}`)
+   }
     render() {
         return (  
         <React.Fragment>
             <p></p>
-            <form className='store-selector'>
-                <h2>Please Enter A Store</h2>
-                <input type='text' required placeholder='Store Name' />
+            <form className='store-selector' onSubmit={this.goToStore}>
+                <h2>Please Enter A Store</h2>     
+                <input type='text' ref={this.myInput} required placeholder='Store Name' defaultValue={getFunName()}/>
                 <button type='submit'>Visit Store →</button>
             </form>
         </React.Fragment>
