20b482fb3cf655e4108329c0a5227f07996167fe
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-17 19:49:14 -0700

    added increase order functionality

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
@@ -25,11 +26,28 @@ class App extends React.Component {
                fishes: sampleFishes
           })
      }
+
+     addToOrder = (key) => {
+          //take a copy of state
+          const order = { ...this.state.order }
+          //either add to the order, or update our current quantity
+          order[key] = order[key] + 1 || 1;
+          //call setState to update our state object
+          this.setState({ order })
+     }
+
      render() {
           return (
                <div className="catch-of-the-day">
                    <div className='menu'>
                         <Header tagline='Fresh Seafood Market'/>
+                        <ul className='fishes'>
+                             {
+                                  Object.keys(this.state.fishes).map((key) => {
+                                   return <Fish index={key} key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+                                  })
+                             }
+                        </ul>
                    </div>
                    <Order />
                    <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
